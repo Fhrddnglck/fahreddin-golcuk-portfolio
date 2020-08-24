799ce5bfc9c8e206fb7bc50f287612c65f93f286
fahreddin <fahreddinglck97@gmail.com>
2020-08-25 00:48:33 +0300

    Updates

---
+++ precache-manifest.9b1780f51fa129f206fac2e68cdaca45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7bd85c913036c3e4c914831780a976",
+    "url": "/fahreddin-golcuk-portfolio/index.html"
+  },
+  {
+    "revision": "f80ed7f8454cf74722da",
+    "url": "/fahreddin-golcuk-portfolio/static/css/main.1b0b1853.chunk.css"
+  },
+  {
+    "revision": "ff67d09129d69d50a5c2",
+    "url": "/fahreddin-golcuk-portfolio/static/js/2.6e84930b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fahreddin-golcuk-portfolio/static/js/2.6e84930b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80ed7f8454cf74722da",
+    "url": "/fahreddin-golcuk-portfolio/static/js/main.5949f055.chunk.js"
+  },
+  {
+    "revision": "e3d25f309fa2de6091f3",
+    "url": "/fahreddin-golcuk-portfolio/static/js/runtime-main.25d80629.js"
+  },
+  {
+    "revision": "0602a72d2d617f75dbcd1dfceace3c74",
+    "url": "/fahreddin-golcuk-portfolio/static/media/background-header.0602a72d.jpg"
+  },
+  {
+    "revision": "1cb9eafb21648fb411eb22b64bbcf314",
+    "url": "/fahreddin-golcuk-portfolio/static/media/logo-faho.1cb9eafb.png"
+  },
+  {
+    "revision": "eccb3c09e835473275669b3d8cc9d93a",
+    "url": "/fahreddin-golcuk-portfolio/static/media/my-photo.eccb3c09.jpeg"
+  }
+]);
